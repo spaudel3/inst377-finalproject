acf7b94aafe75ab63b69ec5b2469b5c449e33635
Shahil Paudel <shahilpaudel@gmail.com>
2025-05-14 16:30:03 -0400

    More work on database integration

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
-// express -v not working even though I have it
 const supabaseClient = require(`@supabase/supabase-js`);
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+// const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseUrl = 'https://tdhnnczikumtzywdgdsq.supabase.co';
+// const supabaseKey = process.env.SUPABASE_KEY;
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkaG5uY3ppa3VtdHp5d2RnZHNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxNTk0NjEsImV4cCI6MjA2MjczNTQ2MX0.DMWY9CV-lZnvFYrCmjiGw91aOIfBHFLjkOq_bdP5ufA';
 const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
 
@@ -17,8 +20,36 @@ app.get('/users', async (req, res) => {
 
     const { data, error } = await supabase.from('user').select();
 
+    if(error) {
+        console.log(`Error: ${error}`);
+        res.statusCode = 500;
+        res.send(error);
+    }
+
     res.send(data)
 });
+
+app.post('/user', async (req, res) => {
+    console.log('Adding User')
+
+    console.log(req.body);
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const monster = req.body.monster;
+
+    const { data, error } = await supabase
+    .from('user')
+    .insert({ user_first_name: firstName, user_last_name: lastName, user_favorite_hero: monster })
+    .select()
+
+    if(error) {
+        console.log(`Error: ${error}`);
+        res.statusCode = 500;
+        res.send(error);
+    }
+
+    res.send(data);
+})
 
 app.listen(port, () => {
     console.log('App is active on port', port);
--- records_home.js
+++ records_home.js
@@ -1,23 +1,35 @@
 // const api_key = "http://comicvine.gamespot.com/api/character/4005-24694/?api_key=7c2192ec41cd4b66a7bd80c9915a0ffed53d33cb"
 // using superhero api instead now, api key is: c5c0b8730b9f3fb24591704b08e84b84
 
-async function sliderImage() {
-    const div = document.getElementById("imageSlider");
-    const id_1 = getRandom();
-    const id_2 = getRandom();
-    const id_3 = getRandom();
-    console.log(id_1, id_2, id_3);
+// function sliderImage() {
+//     const div = document.getElementById("imageSlider");
+//     const id_1 = getRandom();
+//     const id_2 = getRandom();
+//     const id_3 = getRandom();
+//     console.log(id_1, id_2, id_3);
+//     const images = {one:"", two:""}
+//     for (const i in images) {
+//         console.log(i);
+//         console.log(i.value);
+//         let image = document.createElement("img");
+//         image.src = i.value;
+//         image.width = 612;
+//         image.height = 612;
+//         div.appendChild(image);
+//     }
+    
 
-    const api_url = `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/c5c0b8730b9f3fb24591704b08e84b84/${id_1}`;
-    await fetch(api_url)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        let image = document.createElement("img");
-        image.src = data.image;
-        image.width = 612;
-        image.height = 612;
-        div.appendChild(image);
+    // const api_url = `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/c5c0b8730b9f3fb24591704b08e84b84/${id_1}`;
+    // await fetch(api_url)
+    // .then(res => res.json())
+    // .then(data => {
+    //     console.log(data);
+    //     let image = document.createElement("img");
+    //     image.src = data.image;
+    //     image.width = 612;
+    //     image.height = 612;
+    //     div.appendChild(image);
+
         // data.results.forEach((item) => {
         //     let image = document.createElement("img");
         //     image.src = item.image;
@@ -25,9 +37,7 @@ async function sliderImage() {
         //     image.height = 612;
         //     div.appendChild(image);
         // })
-    })
-    simpleslider.getSlider();
-}
+    // }
 
 // async function getAPI() {
 //     // const api_url = `https://superheroapi.com/api/c5c0b8730b9f3fb24591704b08e84b84/search`
@@ -36,8 +46,8 @@ async function sliderImage() {
 
 // }
 
-function getRandom() {
-    min = 1;
-    max = 731;
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// function getRandom() {
+//     min = 1;
+//     max = 731;
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
